26b2311bbc9269fc98ac159a90648f5e9527c1da
Irina Babenko <babenko8irina@gmail.com>
2024-09-13 23:07:05 +0300

    bug fixes in NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import css from './PageNotFoundPage.module.css';
+import css from './NotFoundPage.module.css';
 
 const PageNotFoundPage = () => {
   return (
